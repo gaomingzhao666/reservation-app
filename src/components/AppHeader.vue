fcacee13fe1ce4c7575186b85721ebec0b40421b
Nano <gaomingzhao666@outlook.com>
2025-05-05 14:26:47 +0900

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <h1>header</h1>
+  </header>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <h1>navbar</h1>
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
