97f69ff2ddae7c5ff5da2323960dbc68433dbdc6
Nano <gaomingzhao666@outlook.com>
2025-05-09 20:08:20 +0900

    chore: add links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <h1>navbar</h1>
+  <nav class="flex justify-start items-center space-x-2 p-4">
+    <router-link to="/admin">Management</router-link>
+    <router-link to="/signup">Sign Up</router-link>
+    <router-link to="/login">Login</router-link>
+
+    <aside>
+      <!-- burger icon -->
+    </aside>
   </nav>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
