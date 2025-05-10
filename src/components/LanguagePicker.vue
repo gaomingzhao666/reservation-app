f673203249db104213d3cad11f03b28d50d83e6f
Nano <gaomingzhao666@outlook.com>
2025-05-10 21:52:58 +0900

    feat: add `LanguagePicker.vue`

---
+++ src/components/LanguagePicker.vue
@@ -0,0 +1,26 @@
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <Languages />
+    </DropdownMenuTrigger>
+
+    <DropdownMenuContent>
+      <DropdownMenuItem>English</DropdownMenuItem>
+      <DropdownMenuItem>日本語</DropdownMenuItem>
+      <DropdownMenuItem>简体中文</DropdownMenuItem>
+      <DropdownMenuItem>繁體中文</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<script lang="ts" setup>
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Languages } from 'lucide-vue-next'
+</script>
+
+<style></style>
