07e4483e47cd16f3391194bd71453850d43e5415
Nano <gaomingzhao666@outlook.com>
2025-05-22 11:17:20 +0900

    chore: add `Sonner` component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import { Toaster } from '@/components/ui/sonner'
 </script>
 
 <template>
+  <Toaster richColors :expand="true" position="bottom-right" />
+
   <Header />
-
   <RouterView />
-
   <Footer />
 </template>
 
