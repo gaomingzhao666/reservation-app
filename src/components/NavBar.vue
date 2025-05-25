e63d06fbd1d139248e5f10a08983f0ba303f6aba
Nano <gaomingzhao666@outlook.com>
2025-05-25 19:18:53 +0900

    refactor: change router syntax naming rule from `router-link` to `RouterLink`

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="flex justify-start items-center space-x-4 p-4">
-    <router-link to="/admin">Management</router-link>
-    <router-link to="/signup">Sign Up</router-link>
-    <router-link to="/login">Login</router-link>
+    <RouterLink to="/admin">Management</RouterLink>
+    <RouterLink to="/signup">Sign Up</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
 
     <aside>
       <!-- burger icon -->
