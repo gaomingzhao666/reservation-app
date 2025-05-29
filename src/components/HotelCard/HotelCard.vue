df6e49a538d9e35c698b24e343b635b1972141b7
Nano <gaomingzhao666@outlook.com>
2025-05-29 18:57:29 +0900

    feat: component `HotelCard.vue` for displaying hotel information and booking options

--- src/components/HotelCard/HotelCard.vue
+++ src/components/HotelCard/HotelCard.vue
@@ -2,13 +2,30 @@
   <section>
     <HotelShower />
 
-    <HotelInfoCard />
+    <HotelInfoCard :data="props.data" />
   </section>
 </template>
 
 <script lang="ts" setup>
 import HotelShower from '@/components/HotelCard/HotelShower.vue'
 import HotelInfoCard from '@/components/HotelCard/HotelInfoCard.vue'
+
+interface Props {
+  data: {
+    name: string
+    reviewStars: number
+    isOpening: boolean
+    openTime: string
+    closeTime: string
+    address: string
+    openingDate: string
+    closingDate: string
+    paymentMethods: string[]
+    price: number
+  }
+}
+
+const props = defineProps<Props>()
 </script>
 
 <style></style>
--- src/components/HotelCard/HotelShower.vue
+++ src/components/HotelCard/HotelShower.vue
@@ -1,7 +1,43 @@
 <template>
-  <div></div>
+  <section>
+    <h1>222</h1>
+    <section class="flex items-center justify-center">
+      <video
+        :src="hotelVideo"
+        class="w-xl h-[434px] rounded-xl object-cover"
+        :controls="isControls"
+        @click="isControls = true"
+      ></video>
+
+      <ul class="grid grid-cols-2 gap-4 border ml-4">
+        <li>
+          <img :src="hotelImg1" class="h-52 w-64 rounded-xl" alt="" />
+        </li>
+        <li>
+          <img :src="hotelImg2" class="h-52" alt="" />
+        </li>
+        <li>
+          <img :src="hotelImg3" class="h-52" alt="" />
+        </li>
+        <li>
+          <img :src="hotelImg4" class="h-52" alt="" />
+        </li>
+        <!-- <li>
+          <img :src="hotelImg5" alt="" />
+        </li> -->
+      </ul>
+    </section>
+  </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import hotelVideo from '@/assets/images/hotel-video.mp4'
+import hotelImg1 from '@/assets/images/hotel-1.jpg'
+import hotelImg2 from '@/assets/images/hotel-2.jpg'
+import hotelImg3 from '@/assets/images/hotel-3.jpg'
+import hotelImg4 from '@/assets/images/hotel-4.jpg'
+import hotelImg5 from '@/assets/images/hotel-5.jpg'
+import { ref, type Ref } from 'vue'
 
-<style></style>
+const isControls: Ref<boolean> = ref(false)
+</script>
