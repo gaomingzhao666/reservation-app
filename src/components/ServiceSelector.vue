847e49d90a78425251f797f346a9fc273af08241
Nano <gaomingzhao666@outlook.com>
2025-06-09 11:14:01 +0900

    test: `ButtonGroup.vue`

---
+++ src/components/ServiceSelector.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Select Service</h1>
+
+    <ButtonGroup
+      :data="{
+        categories: ['Room', 'Room', 'Room', 'Room', 'Room', 'selectedRoom'],
+        selectedCategory: 'selectedRoom',
+      }"
+    />
+  </section>
+</template>
+
+<script lang="ts" setup>
+import ButtonGroup from '@/components/ButtonGroup.vue'
+</script>
+
+<style></style>
