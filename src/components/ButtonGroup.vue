ea7901d113a19b5e805663198df942212657a566
Nano <gaomingzhao666@outlook.com>
2025-06-09 09:46:43 +0900

    feat: `ButtonGroup.vue` component for select services or actions

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in props.data.categories" :key="index">
+      <Button :variant="item === props.data.selectedCategory ? 'secondary' : 'outline'">
+        {{ item }}
+      </Button>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  data: {
+    categories: string[]
+    selectedCategory: string
+  }
+}
+const props = defineProps<Props>()
+</script>
+
+<style></style>
