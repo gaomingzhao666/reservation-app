3e0c3a72cc27fafe2af6efd1c490020c6cd1456c
Nano <gaomingzhao666@outlook.com>
2025-06-22 21:02:35 +0900

    chore(porting): replace `supabase` by `firebase`

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDhzoOv_Nd7tNhSBBWqVC2YSyiEZ0Cg4MI',
+  authDomain: 'learn-firebase-8b54d.firebaseapp.com',
+  projectId: 'learn-firebase-8b54d',
+  storageBucket: 'learn-firebase-8b54d.firebasestorage.app',
+  messagingSenderId: '1098979221481',
+  appId: '1:1098979221481:web:83a210f7a7d245ba5fb578',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
