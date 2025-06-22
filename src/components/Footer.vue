a80654ea278743a528ce7f2653ba9323c7661327
Nano <gaomingzhao666@outlook.com>
2025-06-22 21:01:08 +0900

    add `Footer` section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
-    <h1>footer</h1>
-  </div>
+  <footer>
+    <h2 class="text-sm text-center p-10 border rounded-t-4xl">
+      @Copyright {{ new Date().getFullYear() }} Reservation App built by
+      <a href="https://github.com/gaomingzhao666" class="underline">Nano</a>
+    </h2>
+  </footer>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
