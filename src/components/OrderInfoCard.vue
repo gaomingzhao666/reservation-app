983aad747d751ff725327f51f6faa177dc89150f
Nano <gaomingzhao666@outlook.com>
2025-06-23 11:05:31 +0900

    feat: basic Order Information Card section style completed

---
+++ src/components/OrderInfoCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <Card>
+    <CardHeader>
+      <h2>Order Information</h2>
+
+      <section>
+        <img :src="serviceImageUrl" alt="Selected Service's Image" />
+
+        <h4>{{ serviceProviderName }}</h4>
+      </section>
+    </CardHeader>
+
+    <CardContent class="flex justify-between items-center">
+      <h4>{{ serviceTitle }}</h4>
+      <p>{{ servicePrice }}</p>
+    </CardContent>
+
+    <CardContent class="flex justify-between items-center">
+      <h4>Service Duration</h4>
+      <p>{{ serviceDuration }}</p>
+    </CardContent>
+
+    <CardContent class="flex justify-between items-center">
+      <h4>Service Date</h4>
+      <p>{{ serviceDateRange }}</p>
+    </CardContent>
+
+    <CardFooter>
+      <Button>Continue</Button>
+    </CardFooter>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card'
+
+// it's not be filled, we need that schema types from supabase dashboard but we don't have create the project in dashboard yet
+// here we just define the types for the service section and fill the styles and component just for service information
+interface Props {
+  data: {
+    // for service
+    serviceImageUrl: string
+    serviceProviderName: string
+    serviceTitle: string
+    serviceDuration: string
+    servicePrice: string
+    serviceDateRange: string
+  }
+}
+const props = defineProps<Props>()
+const {
+  serviceImageUrl,
+  serviceProviderName,
+  serviceTitle,
+  serviceDuration,
+  servicePrice,
+  serviceDateRange,
+} = props.data
+</script>
--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,9 +1,41 @@
 <template>
   <div>
-    <h1>Booking Page</h1>
+    <h2>Select Service</h2>
+
+    <main class="flex flex-col md:flex-row justify-around items-center">
+      <section class="flex flex-col gap-y-4">
+        <ServiceSelector />
+        <ServiceItemSelectList />
+      </section>
+
+      <aside class="sticky top-0 right-0 w-full max-w-sm">
+        <OrderInfoCard
+          :data="{
+            serviceImageUrl: 'https://picsum.photos/200',
+            serviceProviderName: 'John Doe',
+            serviceTitle: 'Haircut',
+            serviceDuration: '1 hour',
+            servicePrice: '$20.00',
+            serviceDateRange: '2024-01-01 - 2024-01-01',
+          }"
+        />
+      </aside>
+    </main>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+
+import ServiceSelector from '@/components/ServiceSelector.vue'
+import ServiceItemSelectList from '@/components/ServiceItemSelectList.vue'
+import OrderInfoCard from '@/components/OrderInfoCard.vue'
+const route = useRoute()
+
+watchEffect(async () => {
+  const res = route.params.id
+})
+</script>
 
 <style></style>
