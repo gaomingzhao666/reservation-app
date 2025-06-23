5e7f5a872763e8ec2ebab0429d3b39487f9f5c49
Nano <gaomingzhao666@outlook.com>
2025-06-23 10:42:51 +0900

    chore: `<a>` adjust to open a new tab

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer>
-    <h2 class="text-sm text-center p-10 border rounded-t-4xl">
+    <h2 class="text-center p-10 border rounded-t-4xl">
       @Copyright {{ new Date().getFullYear() }} Reservation App built by
-      <a href="https://github.com/gaomingzhao666" class="underline">Nano</a>
+      <a href="https://github.com/gaomingzhao666" class="underline" target="_blank">Nano</a>
     </h2>
   </footer>
 </template>
