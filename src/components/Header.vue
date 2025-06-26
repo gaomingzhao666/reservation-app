7b08df397c1e29e25f99026921187cc250f2045d
Nano <gaomingzhao666@outlook.com>
2025-06-26 22:22:41 +0900

    chore(style): responsive `Header.vue` component burger menu for mobile devices

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header class="p-5 flex items-center justify-between">
+    <!-- logo -->
     <section class="flex items-center justify-start space-x-4">
       <RouterLink to="/index">
         <h1 class="text-xl font-semibold">Reservation App</h1>
@@ -8,6 +9,7 @@
       <Input type="text" placeholder="Search by place" class="w-full md:w-80" />
     </section>
 
+    <!-- navbar & controllers -->
     <section class="flex items-center justify-end">
       <Navbar />
 
@@ -16,14 +18,53 @@
         <LanguagePicker />
       </section>
     </section>
+
+    <!-- menu that wrapped navbar and controllers for mobile -->
+    <Drawer>
+      <DrawerTrigger class="block md:hidden"><Menu /></DrawerTrigger>
+      <DrawerContent class="h-full p-5">
+        <section class="flex items-center justify-between my-8">
+          <h2 class="text-xl font-semibold">Navigation</h2>
+
+          <section class="flex items-center justify-start space-x-4">
+            <ThemeToggle />
+            <LanguagePicker />
+          </section>
+        </section>
+
+        <Input type="text" placeholder="Search by place" class="w-full p-5 mb-5" />
+
+        <RouterLink
+          :to="item.trim().toLowerCase()"
+          v-for="(item, index) in ['Management', 'Sign Up', 'Sign in']"
+          :key="index"
+        >
+          <Button variant="outline" class="w-full my-2 p-5">{{ item }}</Button>
+        </RouterLink>
+
+        <DrawerFooter> </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   </header>
 </template>
 
 <script lang="ts" setup>
 import Navbar from '@/components/Navbar.vue'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 import LanguagePicker from '@/components/LanguagePicker.vue'
 import ThemeToggle from '@/components/ThemeToggle.vue'
+import { Menu } from 'lucide-vue-next'
 </script>
 
 <style></style>
