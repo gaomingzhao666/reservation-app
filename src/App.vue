2e16c5b5c71dd9e5e0c6443b56447563a1055583
Nano <gaomingzhao666@outlook.com>
2025-06-30 11:41:06 +0900

    fix: the html lang attribute doesn't change respect to the i18n configurationhttps://github.com/gaomingzhao666/reservation-app/issues/2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { watch } from 'vue'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import { Toaster } from '@/components/ui/sonner'
+
+import { useI18n } from 'petite-vue-i18n'
+
+const { locale } = useI18n()
+watch(
+  () => locale.value,
+  (newVal) => {
+    // set the lang attribute depending on the new locale value
+    document.getElementsByTagName('html')[0].setAttribute('lang', newVal)
+  },
+)
 </script>
 
 <template>
@@ -12,5 +24,3 @@ import { Toaster } from '@/components/ui/sonner'
   <RouterView />
   <Footer />
 </template>
-
-<style scoped></style>
