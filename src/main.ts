6067220f680daccd502040f2692c27cf281a8f5a
Nano <gaomingzhao666@outlook.com>
2025-06-30 11:15:17 +0900

    feat: add i18n feature

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { createI18n } from 'petite-vue-i18n'
+import enUS from './i18n/ui/enUS.json'
+import ja from './i18n/ui/ja.json'
+
+const i18n = createI18n({
+  // enable composition api
+  legacy: false,
+  fallbackLocale: 'en',
+  messages: {
+    English: enUS,
+    日本語: ja,
+  },
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
