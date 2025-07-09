69c0179da7532032d1b0a7ea2662b6aed54565d3
Nano <gaomingzhao666@outlook.com>
2025-07-09 22:21:35 +0800

    feat: user type, login logout logic, and responsive layout optimizations

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,78 @@
 <template>
   <nav class="flex flex-col items-start justify-start space-x-2 p-4 md:flex-row">
+    <Button variant="secondary" class="my-2 w-full p-5 md:hidden">{{ username }}</Button>
+
     <RouterLink to="/admin" class="w-full cursor-pointer">
-      <button class="mx-2 hidden cursor-pointer md:block">Admin</button>
+      <Button variant="ghost" class="mx-2 hidden cursor-pointer md:block">Admin</Button>
       <Button variant="outline" class="my-2 w-full p-5 md:hidden">Admin</Button>
     </RouterLink>
 
-    <section v-if="true" class="w-full space-x-2 md:flex">
-      <RouterLink to="/signup" class="w-full">
-        <button class="mx-2 hidden cursor-pointer text-nowrap md:block">Sign Up</button>
-        <Button :variant="'outline'" class="my-2 w-full p-5 md:hidden">Sign Up</Button>
+    <section class="w-full space-x-2 md:flex">
+      <RouterLink v-if="!isLoggedIn" to="/signup" class="w-full">
+        <Button variant="ghost" class="mx-2 hidden cursor-pointer text-nowrap md:block"
+          >Sign Up</Button
+        >
+        <Button variant="outline" class="my-2 w-full p-5 md:hidden">Sign Up</Button>
       </RouterLink>
 
-      <RouterLink to="/login" class="w-full cursor-pointer">
-        <button class="mx-2 hidden cursor-pointer md:block">Login</button>
+      <RouterLink v-if="!isLoggedIn" to="/login" class="w-full cursor-pointer">
+        <Button variant="ghost" class="mx-2 hidden cursor-pointer md:block">Login</Button>
         <Button variant="outline" class="my-2 w-full p-5 md:hidden">Login</Button>
       </RouterLink>
+
+      <Button
+        variant="outline"
+        class="my-2 w-full p-5 md:hidden"
+        v-if="isLoggedIn"
+        @click="logout()"
+        >Logout</Button
+      >
     </section>
 
-    <Button variant="secondary" v-else @click="testApi()">username</Button>
+    <DropdownMenu v-if="isLoggedIn">
+      <DropdownMenuTrigger class="mr-2 hidden md:block">
+        <Button variant="secondary" class="cursor-pointer">{{ username }}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <LogOut class="size-5" />
+          <span @click="logout()">Logout</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
 </template>
 
 <script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
-import { getToken } from '@/lib/token'
+import { LogOut } from 'lucide-vue-next'
+import { ref, type Ref } from 'vue'
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
+import { toast } from 'vue-sonner'
+import { useRouter } from 'vue-router'
 
-const testApi = () => {
-  fetch(`https://www.googleapis.com/oauth2/v2/userinfo?authentication=${getToken()}"`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-    })
+const auth = getAuth()
+const isLoggedIn: Ref<boolean> = ref(auth.currentUser ? true : false)
+const username: Ref<string> = ref(auth.currentUser?.displayName || 'undefined')
+const router = useRouter()
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    isLoggedIn.value = true
+    username.value = user.displayName || 'undefined'
+  } else isLoggedIn.value = false
+})
+
+const logout = () => {
+  signOut(auth).then(() => {
+    toast.success('Logout successfully')
+    router.push('/login')
+  })
 }
 </script>
