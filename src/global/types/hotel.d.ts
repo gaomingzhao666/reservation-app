b704374a140aa2ee71b87891b4eccd30fb9e1a41
Nano <gaomingzhao666@outlook.com>
2025-07-10 11:35:12 +0800

    add `Hotel` type for global uses

---
+++ src/global/types/hotel.d.ts
@@ -0,0 +1,12 @@
+interface HotelInfo {
+  address: string
+  close_time: string
+  closing_date: string
+  hotel_id: string
+  hotel_name: string
+  open_time: string
+  opening_date: string
+  payment_methods: string[]
+  price: number
+  review_stars: number
+}
