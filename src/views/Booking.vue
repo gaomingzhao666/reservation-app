edc516a1b4688a366d04b5b737a6f27bfe79905e
Nano <gaomingzhao666@outlook.com>
2025-07-10 18:59:30 +0800

    feat: add stepper to `Booking` page

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,27 +1,58 @@
 <template>
-  <div>
-    <h2>Select Service</h2>
+  <section class="flex flex-col items-start justify-around md:flex-row">
+    <main class="flex flex-col gap-y-4">
+      <Stepper class="flex w-full items-start gap-2">
+        <StepperItem
+          v-for="step in steps"
+          :key="step.step"
+          v-slot="{ state }"
+          class="relative flex w-full flex-col items-center justify-center"
+          :step="step.step"
+        >
+          <StepperIndicator>
+            <component :is="step.icon" class="size-5" />
+          </StepperIndicator>
 
-    <main class="flex flex-col items-start justify-around md:flex-row">
-      <section class="flex flex-col gap-y-4">
-        <ServiceSelector />
-        <ServiceItemSelectList />
-      </section>
+          <StepperSeparator
+            v-if="step.step !== steps[steps.length - 1].step"
+            class="absolute top-5 right-[calc(-50%+10px)] left-[calc(50%+20px)] block h-0.5 shrink-0 rounded-full bg-muted group-data-[state=completed]:bg-primary"
+          />
 
-      <aside class="sticky top-24 w-full max-w-sm">
-        <OrderInfoCard
-          :data="{
-            serviceImageUrl: 'https://picsum.photos/200',
-            serviceProviderName: 'John Doe',
-            serviceTitle: 'Haircut',
-            serviceDuration: '1 hour',
-            servicePrice: '$20.00',
-            serviceDateRange: '2024-01-01 - 2024-01-01',
-          }"
-        />
-      </aside>
+          <div class="mt-5 flex flex-col items-center text-center">
+            <StepperTitle
+              :class="[state === 'active' && 'text-primary']"
+              class="text-sm font-semibold transition lg:text-base"
+            >
+              {{ step.title }}
+            </StepperTitle>
+            <StepperDescription
+              :class="[state === 'active' && 'text-primary']"
+              class="sr-only text-xs text-muted-foreground transition md:not-sr-only lg:text-sm"
+            >
+              {{ step.description }}
+            </StepperDescription>
+          </div>
+        </StepperItem>
+      </Stepper>
+
+      <h2 class="text-xl font-semibold">Select Service</h2>
+      <ServiceSelector />
+      <ServiceItemSelectList />
     </main>
-  </div>
+
+    <aside class="sticky top-24 w-full max-w-sm">
+      <OrderInfoCard
+        :data="{
+          serviceImageUrl: 'https://picsum.photos/200',
+          serviceProviderName: 'John Doe',
+          serviceTitle: 'Haircut',
+          serviceDuration: '1 hour',
+          servicePrice: '$20.00',
+          serviceDateRange: '2024-01-01 - 2024-01-01',
+        }"
+      />
+    </aside>
+  </section>
 </template>
 
 <script lang="ts" setup>
@@ -31,9 +62,42 @@ import { useRoute } from 'vue-router'
 import ServiceSelector from '@/components/ServiceSelector.vue'
 import ServiceItemSelectList from '@/components/ServiceItemSelectList.vue'
 import OrderInfoCard from '@/components/OrderInfoCard.vue'
+import { Info, CheckCircle2, Truck } from 'lucide-vue-next'
+
+import { Button } from '@/components/ui/button'
+import {
+  Stepper,
+  StepperDescription,
+  StepperItem,
+  StepperSeparator,
+  StepperTitle,
+  StepperIndicator,
+} from '@/components/ui/stepper'
+
 const route = useRoute()
 
 watchEffect(async () => {
   const res = route.params.id
 })
+
+const steps = [
+  {
+    step: 1,
+    title: 'Select Service and Date',
+    description: 'Select the service you want to book',
+    icon: Truck,
+  },
+  {
+    step: 2,
+    title: 'Input Necessary Information',
+    description: 'A few details about you so we can take identification',
+    icon: Info,
+  },
+  {
+    step: 3,
+    title: 'Confirm Overall Information',
+    description: 'Review your order and confirm',
+    icon: CheckCircle2,
+  },
+]
 </script>
