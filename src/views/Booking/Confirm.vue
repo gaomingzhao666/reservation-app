11684fb5450b28d8031d572dd01c0f830d3bd696
Nano <gaomingzhao666@outlook.com>
2025-07-15 15:42:52 +0800

    feat: new `Confirm` page

---
+++ src/views/Booking/Confirm.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2 class="mt-16 w-3xl text-center text-3xl">
+    The process is not finished, please check all of the information on right aside of card, and
+    click the submit button if everything's good
+  </h2>
+</template>
